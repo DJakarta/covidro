f12d2ef28af5b65358a77fa945d416cb2b001f9e
mhlnu <alex@mhl.nu>
2020-08-13 12:48:14 +0100

    Nope. Revert

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ import Percentage from './charts/Percentage'
 import { DailyInfected, DailyTestedInfected } from './charts/PerDay'
 import axios from 'axios'
 
-const API = 'https://localhost:4000/api/'
+const API = 'http://localhost:4000/api/'
 
 class Home extends Component {
 
@@ -21,7 +21,8 @@ class Home extends Component {
 	}
 
 	async getData() {
-		axios.get(API).then(res => {
+		axios.get(API, {
+		  }).then(res => {
 			const data = res.data
 			this.setState({ data: data, isLoading: false })
 		})
