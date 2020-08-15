0d4e845c656ac9e7ea69217bc61c34d15e37fe0a
mhlnu <alex@mhl.nu>
2020-08-15 12:29:57 +0100

    Good luck

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const cors = require('cors')
 const axios = require('axios')
 
 const app = express()
+const port = process.env.PORT || 4000
+const hostname = process.env.HOST || '127.0.0.1'
 
 app.use(cors())
-
-const port = process.env.PORT || 4000
 
 const normaliseList = (data) => {
     const res = [];
@@ -116,16 +116,6 @@ app.get("/api", async (req, res, next) => {
     res.json(data)
 });
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-      
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-
-app.listen(port, () => {
-    console.log("Server running on port 4000");
+app.listen(port, hostname, () => {
+    console.log("Server running on port 4000 " + hostname + ', ' + port);
 });
