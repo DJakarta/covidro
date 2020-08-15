fa0b2094e785e92161dd923079988a51ec0c889a
mhlnu <alex@mhl.nu>
2020-08-15 14:16:14 +0100

    Debug

--- src/server.js
+++ src/server.js
@@ -122,8 +122,8 @@ app.listen(port, hostname, () => {
 });
 
 if (prod) {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../build')));
     app.get('*',(req, res)=>{
-        res.sendFile(path.join(__dirname, 'client/build', 'client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../build', '../build/index.html'));
     })
 }
