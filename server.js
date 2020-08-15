89e3afa1a27d10df45093a3e7e980ef730f09665
DJakarta <djbogdy3x@gmail.com>
2020-08-15 19:12:48 +0300

    Possible fix for express route in deployment

--- server.js
+++ server.js
@@ -119,9 +119,6 @@ app.get("/api", async (req, res) => {
 })
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*',(req, res)=>{
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
 
 app.listen(port, '0.0.0.0', () => {
     console.log("Server running on " + hostname + ':' + port);
